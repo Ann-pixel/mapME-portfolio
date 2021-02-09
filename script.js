dd3c8e27d92a17fdc0dc5fbdefc9a9da5a14938d
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-02-09 15:11:10 -0500

    added map using leaflet and event listeners to the map

--- script.js
+++ script.js
@@ -26,22 +26,37 @@ const inputElevation = document.querySelector(".form__input--elevation");
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      //   console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
       const map = L.map("map").setView(coords, 13);
-
+      //   console.log(map);
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvt) {
+        console.log(mapEvt);
+        const { lat, lng } = mapEvt.latlng;
+        console.log(lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("workout!")
+          .openPopup();
+      });
     },
     function (err) {
       alert("could not get your position");
